db1ab865fdd6829c260eeb536c70a7b83e0daed9
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-01-12 23:04:30 +0200

    Create ImageGalleryItem.jsx

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,21 @@
+import {useState} from "react";
+import { Modal } from '../Modal/Modal';
+import { PropTypes } from 'prop-types';
+
+import styles from  '../ImageGalleryItem/ImageGalleryItem.module.css';
+
+export const ImageGalleryItem =({image})=>  {
+ const [isOpen,setIsModalOpen]=useState(false);    
+    return ( 
+        <>
+            <li className={styles.ImageGalleryItem} onClick={() => setIsModalOpen(true)}>
+                <img src={image.webformatURL} alt="" className={styles.ImageGalleryItem_image}  />
+            </li> 
+            {isOpen && (<Modal onClose={() => setIsModalOpen(false)} imageModal={image.largeImageURL}/>)}
+        </> 
+    )    
+}
+
+ImageGalleryItem.propTypes = { 
+    images: PropTypes.shape(), 
+}
