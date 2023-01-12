597d8c6f88eb2a29a32bc71a0c4a5013310476c2
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-01-12 23:04:22 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,8 +1,6 @@
 
 import { PropTypes } from 'prop-types';
-
-
-import { ImageGalleryItem } from './ImageGalleryItem'
+import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
 import styles  from '../ImageGallery/ImageGallery.module.css';
 
 
