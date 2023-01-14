66cd8ba78089a2bb21305612006981dc10bae157
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-01-14 21:13:51 +0200

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,14 +2,14 @@ import {useState} from "react";
 import { Modal } from '../Modal/Modal';
 import { PropTypes } from 'prop-types';
 
-import styles from  '../ImageGalleryItem/ImageGalleryItem.module.css';
+import styles from  '../ImageGalleryItem/ImageGalleryItem.module.css'
 
 export const ImageGalleryItem =({image})=>  {
  const [isOpen,setIsModalOpen]=useState(false);    
     return ( 
         <>
             <li className={styles.ImageGalleryItem} onClick={() => setIsModalOpen(true)}>
-                <img src={image.webformatURL} alt="" className={styles.ImageGalleryItem_image}  />
+                <img src={image.webformatURL} alt="" className={styles.ImageGalleryItem_image}/>
             </li> 
             {isOpen && (<Modal onClose={() => setIsModalOpen(false)} imageModal={image.largeImageURL}/>)}
         </> 
