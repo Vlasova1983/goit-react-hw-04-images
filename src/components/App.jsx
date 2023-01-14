aa9c0cae52a59d99f1cc1834be3fb461f0484142
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-01-14 21:13:45 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,65 +1,52 @@
-import {Component} from "react";
+import {useState,useEffect} from "react";
 import { Searchbar} from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Loader } from './Loader/Loader';
 import { Button } from './Button/Button';
 
-
 import styles  from './App.module.css';
 import axios from 'axios';
 import Notiflix from 'notiflix';
 
-export class App  extends Component { 
-  state = {
-    value:'',
-    images: [],    
-    isLoading: false, 
-    isLoadMore: false,
-    page:1,   
-  };
-      
-  
+export const App  =()=> {  
+  const [images,setIsImages] = useState([]);   
+  const [value,setIsValue] = useState('');
+  const [page,setIsPage] = useState(1);
+  const [isLoading,setIsLoader] = useState(false);
+  const [isLoadMore,setIsLoadMore] = useState(false); 
 
-  componentDidUpdate(_, prevState) {      
-    const { value, page } = this.state;
-    if (value !== prevState.value || page !== prevState.page) {
-      this.setState({isLoading: true, isLoadMore:true});
-      this.fetchData(value, page );
-    };  
-  }
+  useEffect(()=> {      
+    setIsLoader(true);
+    setIsLoadMore(true);      
+    fetchData(value, page);      
+  },[value,page]);
 
-
-  fetchData = async ({value}, page) => {   
+  const fetchData = async ({value}, page) => {   
     try {
       const response= await axios.get(`https://pixabay.com/api/?key=31294159-be9d27b57dbd5b4db758a00af&q=${value}&image_type=photo&orientation=horizontal&per_page=12&page=${page}`);
       const images = response.data.hits;      
-      this.setState({images:[...this.state.images,...images]});                      
+      setIsImages([...images,...images]);                      
     }
     catch (error) {
       Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
     }
     finally{
-      this.setState({isLoading:false});      
+      setIsLoader(false);    
     }        
-  }    
+  }; 
   
-  handleSubmit = async (value)=>{        
-    this.setState({value: value, images:[], page:1});    
-  };
-
-  onClickLoadMore = () => {    
-    this.setState({page:this.state.page+1});    
-  }
-  
-  render(){
-    const { images, isLoading, isLoadMore} = this.state;
-    return (
-      <div className={styles.App}> 
-        <Searchbar  onSubmit={this.handleSubmit}/>
-        <ImageGallery images={images}/>        
-        {isLoading && <Loader/>}
-        {isLoadMore && <Button onClick={this.onClickLoadMore} />}            
-      </div> 
-    );
-  }  
+  const handleSubmit = async (value)=>{    
+    setIsValue(value)
+    setIsPage(1);
+    setIsImages([]);
+  };  
+      
+  return (
+    <div className={styles.App}> 
+      <Searchbar  onSubmit={handleSubmit}/>
+      <ImageGallery images={images}/>        
+      {isLoading && <Loader/>}
+      {isLoadMore && <Button onClick={()=>setIsPage(prev => prev +1)}/>}            
+    </div> 
+  );   
 };
